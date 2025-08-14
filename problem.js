9eba82a2af230bb222a119cb75aecc71bb9b71a1
Ameen224 <ameen8714228@gmail.com>
2025-08-14 12:20:23 +0530

    two question added

--- problem.js
+++ problem.js
@@ -1,9 +1,7 @@
 
 // create a palindrome for a given n number, extends 5 positions both side 
-
 // Input: n = 3
 // Output: -1 0 1 2 3 2 1 0 -1
-
 function creating(n){
     let a=[]
     let b=n-1
@@ -11,7 +9,7 @@ function creating(n){
         a.push(b)
         b--
     }
-    console.log("palindrome for a given n number, extends 5 positions both side :",a.reverse().join(" ") + " " + n + " " + a.reverse().join(" "))
+    console.log("1. palindrome for a given n number, extends 5 positions both side :",a.reverse().join(" ") + " " + n + " " + a.reverse().join(" "))
 }
 creating(0)
 
@@ -20,7 +18,7 @@ creating(0)
 function getLastElement(arr) {
     return arr[arr.length - 1];
 }
-console.log("The last element of the array is :",getLastElement([1, 2, 3, 4, 5])); // Output: 5
+console.log("2. The last element of the array is :",getLastElement([1, 2, 3, 4, 5])); // Output: 5
 
 
 
@@ -30,7 +28,7 @@ function removeDuplicates(arr) {
     return arr.indexOf(value)===index})
  return unique;
 }
-console.log("The unique array :",removeDuplicates([1,2,2,3,4,3,4,2,5,6,4,5])); 
+console.log("3. The unique array :",removeDuplicates([1,2,2,3,4,3,4,2,5,6,4,5])); 
 
 
 // Check if Two Strings are Anagrams or not in JavaScript
@@ -38,9 +36,9 @@ function angram(str1,str2){
     const one = str1.split('').sort().join('')
     const two = str2.split('').sort().join('')
     if(one===two){
-        console.log(" The word is anagram of each other")
+        console.log("4. The word is anagram of each other")
     }else{
-        console.log("rthe word is not anagram of each other")
+        console.log("4. The word is not anagram of each other")
     }
 }
 angram("one","eno")
@@ -56,7 +54,6 @@ angram("one","eno")
 // Output: [[2, 7], [-2, 11]]
 function array(arr, num) {
    let b=arr.filter((num,index)=> arr.indexOf(num)===index).sort((x,y)=>x-y)
-   console.log(b)
    let a=[]
    for(let i=0;i<b.length;i++){
        for(let j=i;j<b.length;j++){
@@ -65,8 +62,63 @@ function array(arr, num) {
            }
        }
    }
-   console.log(a)
+   console.log("5. The unique array of array :",a)
 }
-
 array([2, 7, 11,11, 15, -2,6,4, 9,-5 ,1,-5,5,5,-5], 10);
 
+
+// Given three arrays, find the common elements in all three arrays.
+function print(one, two, three) {
+    let a = []
+    let b = []
+    
+
+    for (let i = 0; i < one.length; i++) {
+        for (let j = 0; j < two.length; j++) {
+            if (one[i] === two[j]) {
+                a.push(one[i])
+            }
+        }
+    }
+
+    for (let i = 0; i < three.length; i++) {
+        for (let j = 0; j < a.length; j++) {
+            if (three[i] === a[j]) {
+                b.push(a[j])
+            }
+        }
+    }
+  console.log(b)
+  const c= b.filter((value,index)=>{return b.indexOf(value)===index})
+  console.log(c)
+}
+const ar1 = [1, 5, 10, 20,20, 40, 80]
+const ar2 = [6, 7, 20, 80,20, 100]
+const ar3 = [3, 4, 15, 20, 30, 70,80, 80, 120]
+
+print(ar1, ar2, ar3) 
+
+
+
+function mergeIntervals(arr) {
+  arr.sort((x,y)=>x[0]-y[0])
+  console.log(arr)
+    const merged = [arr[0]]; 
+
+    for (let i = 1; i < arr.length; i++) {
+        let last = merged[merged.length - 1];
+        let current = arr[i];
+
+        if (last[1] >= current[0]) {
+            last[1] = current[1];
+        } else {
+            merged.push(current);
+        }
+    }
+
+    return merged;
+}
+
+// Test
+const intervals = [[1,3],[2,8],[8,10],[15,18]];
+console.log(mergeIntervals(intervals)); 
